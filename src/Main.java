ac9a4af4ab515bac79dd9c4c52950b0da958ff1f
idriss92 <idriss2004@hotmail.com>
2014-11-29 16:20:32 +0100

    ajout validation xml en utilisant la dtd

---
+++ src/Main.java
@@ -0,0 +1,238 @@
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+import org.w3c.dom.*;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import javax.xml.XMLConstants;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.sax.SAXSource;
+import javax.xml.transform.stream.StreamResult;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
+
+public class Main {
+	
+	public static void main (String[] args) throws ParserConfigurationException, SAXException, IOException {
+		
+		System.out.println("Cette application vous permet d'effectuer 3 opérations ");
+		
+		System.out.println("Menu");
+		System.out.println("Pour créer une école taper 0");
+		System.out.println("Pour valider un fichier xml à partir du schéma xml taper 1");
+		System.out.println("Pour valider un fichier xml à partir de la dtd taper 2");
+		
+		System.out.println();
+		System.out.println("Entrer votre choix : ");
+		Scanner sc = new Scanner(System.in);
+		int choix = sc.nextInt();
+		
+		switch(choix){
+			
+		case 0 :
+			System.out.println("Choix de création validée");
+			System.out.println();
+			createSchool();
+			break;
+		case 1 :
+			System.out.println("Choix de validation avec .XSD");
+			System.out.println();
+			System.out.println("Entrer le nom du fichier Xml précédé du chemin : ");
+			Scanner recup = new Scanner (System.in);
+			String entree1 = recup.nextLine();
+			System.out.println("Entrer le nom du fichier XSD précédé du chemin : ");
+			Scanner recupe = new Scanner (System.in);
+			String entree2 = recupe.nextLine();
+			validateXMLFileWithXsd(entree1,entree2);
+			break;
+			
+		case 2 : 
+			System.out.println("Choix de validation avec .DTD");
+			System.out.println();
+			System.out.println("Entrer le nom du fichier Xml précédé du chemin : ");
+			Scanner recu = new Scanner (System.in);
+			String entre1 = recu.nextLine();
+			System.out.println("Entrer le nom du fichier DTD précédé du chemin : ");
+			Scanner recue = new Scanner (System.in);
+			String entre2 = recue.nextLine();
+			validateXMLFileWithDtd(entre1,entre2);
+			break;
+			
+		default :
+			System.out.println("Votre choix n'est pas pris en compte. Désole");
+			
+		}
+		
+		//validateXMLFileWithXsd("/Users/sani/Documents/xml/Ecole.xml","/Users/sani/Documents/xml/Ecole.xsd");
+		
+		
+		
+	}
+	
+	static private void createSchool(){
+		
+		
+		try{
+			DocumentBuilderFactory dbfe = DocumentBuilderFactory.newInstance();
+			DocumentBuilder db = dbfe.newDocumentBuilder();
+			Document doc = db.newDocument();
+			
+			//Construction racine
+			Element root = doc.createElement("Ecole");
+			int promo;
+			System.out.println("Entrer le nombre de promo");
+			Scanner sc = new Scanner(System.in);
+			promo = sc.nextInt();
+			
+			for(int i =0; i<promo;i++){
+				Element eltPromo = doc.createElement("promo");
+				System.out.println("Entrer le nom de la promo : ");
+				Scanner s = new Scanner(System.in);
+				String nomPromo = s.nextLine();
+				eltPromo.setAttribute("nom", nomPromo);
+				
+				//boolean decistion = false;
+				
+				System.out.println("Voulez vous creer des éleves ?");
+				Scanner des = new Scanner(System.in);
+				String reponse = des.nextLine();
+				
+				if(reponse =="O" ){
+					return;
+				}
+				else{
+					System.out.println("Combien d'étudiants voulez vous créer : ");
+					Scanner te = new Scanner(System.in);
+					int nombreEleve = te.nextInt();
+					Element listEleve = doc.createElement("Eleves");
+					
+					for(int p = 0; p<nombreEleve; p++){
+						Element eltEleve = doc.createElement("Eleve");
+						System.out.println("Identifiant de l'eleve : ");
+						
+						Scanner dep = new Scanner(System.in);
+						String id = dep.nextLine();
+						eltEleve.setAttribute("identifiant", id);
+						
+						System.out.println("Nom de l'eleve : ");
+						Scanner depa = new Scanner(System.in);
+						String name = depa.nextLine();
+						eltEleve.setTextContent(name);
+						//eltEleve.setAttribute("nom", name);
+						listEleve.appendChild(eltEleve);
+					}
+					eltPromo.appendChild(listEleve);
+				}
+				root.appendChild(eltPromo);
+				
+				
+			}
+			
+			doc.appendChild(root);
+			
+			generateXMLFile(doc,new File("/Users/sani/Documents/Ecole.xml"));
+			
+		}
+		catch(Exception e){
+			System.err.println(e);
+		}
+		
+	}
+	
+	static private void generateXMLFile(Document doc, File file){
+        Source source = new DOMSource(doc);
+        // le rÈsultat de cette transformation sera un flux d'Ècriture dans
+        // un fichier
+        StreamResult resultat = new StreamResult(file);
+         
+        // crÈation du transformateur XML
+        Transformer transfo = null;
+        try {
+            transfo = TransformerFactory.newInstance().newTransformer();
+        } catch(TransformerConfigurationException e) {
+            System.err.println("Impossible de crÈer un transformateur XML.");
+            System.exit(1);
+        }
+         
+        // configuration du transformateur
+         
+        // sortie en XML
+        transfo.setOutputProperty(OutputKeys.METHOD, "xml");
+         
+        // inclut une dÈclaration XML (recommandÈ)
+        transfo.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "no");
+         
+        // codage des caractËres : UTF-8. Ce pourrait Ítre Ègalement ISO-8859-1
+        transfo.setOutputProperty(OutputKeys.ENCODING, "utf-8");
+         
+        // idente le fichier XML
+        transfo.setOutputProperty(OutputKeys.INDENT, "yes");
+         
+        try {
+            transfo.transform(source, (javax.xml.transform.Result) resultat);
+        } catch(TransformerException e) {
+            System.err.println("La transformation a ÈchouÈ : " + e);
+            System.exit(1);
+        }
+
+}
+	
+	
+	static private void validateXMLFileWithXsd(String fichierXml, String schemaXsd) throws SAXException, IOException{		
+		SchemaFactory factory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+		
+		File schemalocate = new File(schemaXsd);
+		
+		Schema schema = factory.newSchema(schemalocate);
+		
+		Validator validator = schema.newValidator();
+		
+		Source source = new StreamSource(fichierXml);
+		
+		try{
+			validator.validate(source);
+			System.out.println("Le fichier "+fichierXml + " est valide par rapport au schéma "+schemaXsd);
+		}
+		catch(SAXException ex){
+			System.out.println(fichierXml + "est invalide car ");
+			System.out.print(ex.getMessage());
+		}
+		
+	}
+
+	static private void validateXMLFileWithDtd(String fichierXml, String dtd) throws SAXException,IOException{
+		//InputSource is = new InputSource(new FileInputStream(fichierXml));
+		//Validator v = new Validator(is);
+		
+		SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+		File schemalocate = new File(dtd);
+		Schema schema = factory.newSchema(schemalocate);
+		Validator validator = schema.newValidator();
+		Source source = new StreamSource(fichierXml);
+		
+		try{
+			validator.validate(source);
+			System.out.println("Le fichier "+fichierXml + " est valide par rapport au schéma "+dtd);
+		}
+		catch(SAXException ex){
+			System.out.println(fichierXml + "est invalide car ");
+			System.out.print(ex.getMessage());
+		}
+	}
+}
